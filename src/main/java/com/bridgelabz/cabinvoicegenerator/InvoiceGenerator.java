448adb32b982a2772861458a6d0d8b086c2e7df7
prem-12345 <premkpatil1997@gmail.com>
2021-09-24 19:47:50 +0530

    Welcome to cab invoice generator

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+
+        //"WELCOME TO CAB INVOICE GENERATOR PROBLEM"
+    }
+}
