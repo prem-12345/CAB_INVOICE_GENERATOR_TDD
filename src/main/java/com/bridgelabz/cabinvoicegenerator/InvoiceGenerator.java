b8228b126577b4455b8cefe584c6e8299965578d
prem-12345 <premkpatil1997@gmail.com>
2021-09-24 19:58:03 +0530

    Added calculate fare method and test case

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
+    private static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5.0;
 
-        //"WELCOME TO CAB INVOICE GENERATOR PROBLEM"
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
---
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        assertEquals(25,fare,0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_shouldReturnMinimumFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        assertEquals(5,fare,0.0);
+    }
+}
